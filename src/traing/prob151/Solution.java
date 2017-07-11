4e3d786b2797f2dd383f3c88e6d4972fd3f788cf
Ghost141 <kidd747@gmail.com>
2017-07-11 18:33:26 +0800

    #151 Reverse string. Done

--- src/traing/prob151/Solution.java
+++ src/traing/prob151/Solution.java
@@ -2,7 +2,38 @@ package traing.prob151;
 
 public class Solution {
     public String reverseWords(String s) {
+        String word = "";
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            final char c = s.charAt(i);
+            if (c == ' ') {
+                if (word.length() > 0) {
+                    if (result.length() > 0) {
+                        word = word + " ";
+                    }
+                    result = word + result;
+                    word = "";
+                }
+            } else {
+                word += c;
+            }
+        }
 
-        return null;
+        if (word.length() > 0) {
+            if (result.length() > 0) {
+                word = word + " ";
+            }
+            result = word + result;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        final Solution s = new Solution();
+
+        System.out.println("|" + s.reverseWords(" ") + "|");
+        System.out.println("|" + s.reverseWords("a") + "|");
+        System.out.println("|" + s.reverseWords("a ") + "|");
     }
 }
