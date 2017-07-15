a21820f2cfe11127a24e930148c08a227feb1eba
Ghost141 <kidd747@gmail.com>
2017-07-15 19:19:59 +0800

    #237 Solved

---
+++ src/traing/prob237/Solution.java
@@ -0,0 +1,12 @@
+package traing.prob237;
+
+import traing.ListNode;
+
+public class Solution {
+    public void deleteNode(ListNode node) {
+        if (node.next != null) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
