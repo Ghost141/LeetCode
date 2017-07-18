8b64fe6f36b3ce597e1dcad63074d418213aa846
Ghost141 <kidd747@gmail.com>
2017-07-18 20:38:05 +0800

    #121 Accepted The solution is O(N). But there is a link I should look into https://en.wikipedia.org/wiki/Maximum_subarray_problem

---
+++ src/traing/prob121/Solution.java
@@ -0,0 +1,33 @@
+package traing.prob121;
+
+/**
+ * Best Time to Buy and Sell Stock.
+ *
+ * @author zhaokai
+ */
+public class Solution {
+
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0 || prices.length == 1) return 0;
+
+        int min = prices[0], max = 0, maxDiff = 0;
+
+        for (int p : prices) {
+            if (p < min) {
+                min = max = p;
+            }
+            max = Math.max(max, p);
+            maxDiff = Math.max(maxDiff, max - min);
+        }
+
+        return maxDiff;
+    }
+
+    public static void main(String[] args) {
+        final Solution s = new Solution();
+
+        System.out.println(s.maxProfit(new int[] {1, 2, 3, 4, 5, 6}));
+        System.out.println(s.maxProfit(new int[] {7, 6, 5, 4, 3, 2, 1}));
+        System.out.println(s.maxProfit(new int[] {7, 1, 5, 3, 6, 4}));
+    }
+}
