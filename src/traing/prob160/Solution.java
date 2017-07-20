a885252ca246ec4b8516e51c5c2a54d3e0e9f115
Ghost141 <kidd747@gmail.com>
2017-07-20 14:26:49 +0800

    #160 Accepted

---
+++ src/traing/prob160/Solution.java
@@ -0,0 +1,24 @@
+package traing.prob160;
+
+import traing.ListNode;
+
+/**
+ * Intersection of Two Linked Lists
+ *
+ * @author zhaokai
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode a = headA, b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+
+}
