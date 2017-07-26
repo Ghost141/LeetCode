94a2030c05130fc04f8c222b433c8d8ec44127d7
Ghost141 <kidd747@gmail.com>
2017-07-26 10:37:54 +0800

    #11 Accepted Solve by checking official solution. The idea is basically greedy solution.

---
+++ src/traing/prob11/Solution.java
@@ -0,0 +1,34 @@
+package traing.prob11;
+
+/**
+ * Container With Most Water
+ * Link:https://leetcode.com/problems/container-with-most-water/#/description
+ *
+ * @author zhaokai
+ */
+public class Solution {
+    public int maxArea(int[] height) {
+        if (height.length < 2) {
+            return 0;
+        }
+        int start = 0, end = height.length - 1;
+        int max = 0;
+        while (start != end) {
+            max = Math.max(max, (end - start) * Math.min(height[start], height[end]));
+            if (height[start] >= height[end]) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        final Solution s = new Solution();
+
+        System.out.println(s.maxArea(new int[] {}));
+        System.out.println(s.maxArea(new int[] {1}));
+        System.out.println(s.maxArea(new int[] {1, 2}));
+    }
+}
