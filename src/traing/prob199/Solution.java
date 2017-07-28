f0ca4c81cb41fc652d3a3af0113cace5ad0397d0
Ghost141 <kidd747@gmail.com>
2017-07-28 22:53:55 +0800

    #199 Accepted It actually can be wrote in for-loop instead of recursion

---
+++ src/traing/prob199/Solution.java
@@ -0,0 +1,37 @@
+package traing.prob199;
+
+import traing.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Binary Tree Right Side View
+ * Link: https://leetcode.com/problems/binary-tree-right-side-view/tabs/description
+ *
+ * @author zhaokai
+ */
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        List<TreeNode> nodeList = new ArrayList<>();
+        nodeList.add(root);
+        return _find(nodeList, result);
+    }
+
+    private List<Integer> _find(List<TreeNode> nodeList, List<Integer> previous) {
+        List<TreeNode> nexLevel = new ArrayList<>();
+        previous.add(nodeList.get(nodeList.size() - 1).val);
+
+        for (TreeNode node : nodeList) {
+            if (node.left != null) nexLevel.add(node.left);
+            if (node.right != null) nexLevel.add(node.right);
+        }
+        if (nexLevel.isEmpty()) {
+            return previous;
+        } else {
+            return _find(nexLevel, previous);
+        }
+    }
+}
