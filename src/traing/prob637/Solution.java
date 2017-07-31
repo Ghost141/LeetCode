5618077d835519410ee4f9a91f286f195b949c26
Ghost141 <kidd747@gmail.com>
2017-07-31 12:04:07 +0800

    #637 Accepted A better space complexity solution based on BFS

--- src/traing/prob637/Solution.java
+++ src/traing/prob637/Solution.java
@@ -3,7 +3,9 @@ package traing.prob637;
 import traing.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Average of Levels in Binary Tree
@@ -14,27 +16,29 @@ import java.util.List;
  */
 public class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
+        if (root == null) return new ArrayList<>();
         List<Double> result = new ArrayList<>();
-        List<Integer> count = new ArrayList<>();
-        dfsCal(root, 0, count, result);
 
-        for (int i = 0; i < result.size(); i++) {
-            result.set(i, result.get(i) / count.get(i));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Queue<TreeNode> temp = new LinkedList<>();
+            long sum = 0, count = 0;
+            while (!queue.isEmpty()) {
+                final TreeNode node = queue.poll();
+                sum += node.val;
+                count++;
+                if (node.left != null) {
+                    temp.add(node.left);
+                }
+                if (node.right != null) {
+                    temp.add(node.right);
+                }
+            }
+            result.add((double) (sum / count));
+            queue = temp;
         }
-
         return result;
     }
 
-    private void dfsCal(TreeNode node, int level, List<Integer> count, List<Double> result) {
-        if (node == null) return;
-        if (level < result.size()) {
-            result.set(level, result.get(level) + node.val);
-            count.set(level, count.get(level) + 1);
-        } else {
-            result.add((double) node.val);
-            count.add(1);
-        }
-        dfsCal(node.left, level + 1, count, result);
-        dfsCal(node.right, level + 1, count, result);
-    }
 }
