7d23123fb9678a170a6d64ac623ab76fc1168f0e
Ghost141 <kidd747@gmail.com>
2017-07-31 21:56:59 +0800

    #319 Accepted

---
+++ src/traing/prob319/Solution.java
@@ -0,0 +1,15 @@
+package traing.prob319;
+
+/**
+ * Bulb Switcher
+ * Link: https://leetcode.com/problems/bulb-switcher/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 7/31/17
+ */
+public class Solution {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
