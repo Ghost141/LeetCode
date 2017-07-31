4699d935d0f287838d465bb7fefb48959d4b582a
Ghost141 <kidd747@gmail.com>
2017-07-31 17:14:05 +0800

    #226 Accepted

---
+++ src/traing/prob226/Solution.java
@@ -0,0 +1,23 @@
+package traing.prob226;
+
+import traing.TreeNode;
+
+/**
+ * Invert Binary Tree
+ * Link: https://leetcode.com/problems/invert-binary-tree/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 7/31/17
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode l = root.left, r = root.right;
+        root.left = invertTree(r);
+        root.right = invertTree(l);
+
+        return root;
+    }
+
+}
