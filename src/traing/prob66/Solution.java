abcbbe9ba41697c267326e33b4119818a8f62bcc
Ghost141 <kidd747@gmail.com>
2017-07-31 19:50:02 +0800

    #66 Accepted

---
+++ src/traing/prob66/Solution.java
@@ -0,0 +1,41 @@
+package traing.prob66;
+
+import traing.Utils;
+
+/**
+ * Plus One
+ * Link: https://leetcode.com/problems/plus-one/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 7/31/17
+ */
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        if (digits[n - 1] < 9) {
+            digits[n - 1]++;
+            return digits;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newRes = new int[n + 1];
+        newRes[0] = 1;
+
+        return newRes;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+//        Utils.printai(s.plusOne(new int[] {1}));
+        Utils.printai(s.plusOne(new int[] {9}));
+    }
+}
