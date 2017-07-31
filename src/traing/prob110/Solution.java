b55efd15ea6a55c5340d9755507766ae1e9f73c4
Ghost141 <kidd747@gmail.com>
2017-07-31 12:30:15 +0800

    #110 Accepted

---
+++ src/traing/prob110/Solution.java
@@ -0,0 +1,29 @@
+package traing.prob110;
+
+import traing.TreeNode;
+
+/**
+ * Balanced Binary Tree
+ * Link: https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 7/31/17
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) return 0;
+        final int heightLeft = height(node.left);
+        if (heightLeft != -1) {
+            final int heightRight = height(node.right);
+            if (heightRight != -1) {
+                return Math.abs(heightRight - heightLeft) > 1 ? -1 : (Math.max(heightRight, heightLeft) + 1);
+            }
+        }
+        return -1;
+    }
+}
