25907675821165c23ab08a865036b0e36655ccc5
Ghost141 <kidd747@gmail.com>
2017-07-31 11:22:05 +0800

    #107 Accepted

---
+++ src/traing/prob107/Solution.java
@@ -0,0 +1,54 @@
+package traing.prob107;
+
+import traing.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Binary Tree Level Order Traversal II
+ * Link: https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+ *
+ * @author zhaokai
+ */
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<TreeNode> nodeList = new ArrayList<>();
+        nodeList.add(root);
+
+        Stack<List<Integer>> stack = new Stack<>();
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> levelValue = new ArrayList<>();
+
+        int start = 0, end = 1, level = 1;
+
+        while (start != end) {
+            final TreeNode node = nodeList.get(start);
+            end += add(nodeList, node.left);
+            end += add(nodeList, node.right);
+            levelValue.add(node.val);
+            if (++start == level) {
+                stack.push(levelValue);
+                levelValue = new ArrayList<>();
+                level = end;
+            }
+        }
+
+        while (!stack.empty()) {
+            result.add(stack.pop());
+        }
+
+        return result;
+    }
+
+    private int add(List<TreeNode> list, TreeNode node) {
+        if (node != null) {
+            list.add(node);
+            return 1;
+        }
+        return 0;
+    }
+
+}
