54b23a2d9f36514f3fff4e1b63daa7ab1bdba2be
Ghost141 <kidd747@gmail.com>
2017-08-03 13:57:45 +0800

    #263 Accepted

--- src/traing/prob263/Solution.java
+++ src/traing/prob263/Solution.java
@@ -10,6 +10,7 @@ package traing.prob263;
  */
 public class Solution {
     public boolean isUgly(int num) {
+        if (num <= 0) return false;
         for (int i = 2; i < 6; i++ )
             while (num % i == 0)
                 num /= i;
