9c09691cb146847428d86cd362016156193f8db1
Ghost141 <kidd747@gmail.com>
2017-08-03 13:57:45 +0800

    #263 Accepted

---
+++ src/traing/prob263/Solution.java
@@ -0,0 +1,18 @@
+package traing.prob263;
+
+/**
+ * Ugly Number
+ * Link: https://leetcode.com/problems/ugly-number/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 8/1/17
+ */
+public class Solution {
+    public boolean isUgly(int num) {
+        for (int i = 2; i < 6; i++ )
+            while (num % i == 0)
+                num /= i;
+        return num == 1;
+    }
+}
