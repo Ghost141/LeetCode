8e3feb05036f25d1ed51c1673fab0fae1ea38117
Ghost141 <kidd747@gmail.com>
2017-08-08 00:49:48 +0800

    #69 Accepted Fix bug in previous implementation

--- src/traing/prob69/Solution.java
+++ src/traing/prob69/Solution.java
@@ -11,16 +11,18 @@ package traing.prob69;
 public class Solution {
     public int mySqrt(int x) {
         if (x <= 0) return 0;
-        int start = 0, end = x / 2, middle;
+        if (x <= 3) return 1;
+        int start = 0, end = x / 2 + 1, middle;
 
         while (true) {
             middle = (start + end) / 2;
             int res = x / middle;
-            if (res == middle) {
-                return middle;
-            } else if (res > middle) {
-                start = middle;
-            } else {
+            if (res > middle) {
+                start = middle + 1;
+            } else if (res <= middle) {
+                if (x / (res + 1) < res + 1) {
+                    return res;
+                }
                 end = middle;
             }
         }
@@ -30,11 +32,22 @@ public class Solution {
 //        System.out.println(Math.sqrt(Integer.MAX_VALUE));
 //        System.out.println((int) Math.sqrt(Integer.MAX_VALUE));
         Solution s = new Solution();
-
+//
         System.out.println(s.mySqrt(Integer.MAX_VALUE - 50000));
         System.out.println(Math.sqrt(Integer.MAX_VALUE - 50000));
 
         System.out.println(s.mySqrt(46339*46339));
+        System.out.println(Math.sqrt(46339*46339));
+        System.out.println(s.mySqrt(1));
+        System.out.println(s.mySqrt(2));
+        System.out.println(s.mySqrt(3));
+        System.out.println(s.mySqrt(4));
+        System.out.println(s.mySqrt(5));
+        System.out.println(s.mySqrt(6));
+        System.out.println(s.mySqrt(7));
+        System.out.println(s.mySqrt(8));
+        System.out.println(s.mySqrt(9));
+        System.out.println(s.mySqrt(10));
         System.out.println(Math.sqrt(46339*46339));
 //        System.out.println(Integer.MAX_VALUE - 50000);
     }
