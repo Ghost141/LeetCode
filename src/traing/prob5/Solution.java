6a4ecc9cf2ef2f60a9f7a8929b6f2064112c9176
Ghost141 <kidd747@gmail.com>
2017-08-07 16:26:17 +0800

    #5 Accepted The expand around center Solution. Time Complexity: O(N^2) Space Complexity: O(1)

--- src/traing/prob5/Solution.java
+++ src/traing/prob5/Solution.java
@@ -35,9 +35,32 @@ public class Solution {
         return s.substring(start, end);
     }
 
+    private String expandAroundCenter(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int maxLen = Math.max(len1, len2);
+            if (maxLen > end - start) {
+                start = i - (maxLen - 1) / 2;
+                end = i + maxLen / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
 
         System.out.println(s.longestPalindrome("babad"));
+        System.out.println(3 / 2);
     }
 }
