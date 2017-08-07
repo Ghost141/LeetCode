8941291dc4f1db6547db9165285a4258adec880d
Ghost141 <kidd747@gmail.com>
2017-08-08 01:52:10 +0800

    Public TreeLinkNode class

---
+++ src/traing/TreeLinkNode.java
@@ -0,0 +1,17 @@
+package traing;
+
+/**
+ * The public tree link node class.
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 8/8/17
+ */
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+
+    public TreeLinkNode(int x) {
+        val = x;
+    }
+}
