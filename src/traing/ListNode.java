9556cd9f77c346306dd414df73e322d3c7336758
Ghost141 <kidd747@gmail.com>
2017-08-09 22:01:15 +0800

    Upgrade ListNode class

--- src/traing/ListNode.java
+++ src/traing/ListNode.java
@@ -9,4 +9,14 @@ public class ListNode {
     public ListNode(int val) {
         this.val = val;
     }
+
+    public ListNode (int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return next == null ? String.valueOf(val) : String.valueOf(val) + "->" + next.toString();
+    }
 }
