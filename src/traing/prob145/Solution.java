fcfeec04b02f1ba93f7dabf11c3f2d2f46f2583d
Ghost141 <kidd747@gmail.com>
2017-08-14 15:36:11 +0800

    #145 Accepted Iterative solution

--- src/traing/prob145/Solution.java
+++ src/traing/prob145/Solution.java
@@ -2,8 +2,7 @@ package traing.prob145;
 
 import traing.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Binary Tree Postorder Traversal
@@ -25,5 +24,32 @@ public class Solution {
         postorder(node.right, list);
         list.add(node.val);
         return list;
+    }
+
+    private List<Integer> iterative(TreeNode node) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        List<Integer> res = new ArrayList<>();
+        TreeNode cur = node;
+        while (!stack.isEmpty() || cur != null) {
+            while (!isLeaf(cur)) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            if (cur != null) res.add(cur.val);
+            while (!stack.isEmpty() && cur == stack.peek().right) {
+                cur = stack.pop();
+                res.add(cur.val);
+            }
+            if (stack.isEmpty()) {
+                cur = null;
+            } else {
+                cur = stack.peek().right;
+            }
+        }
+        return res;
+    }
+
+    private boolean isLeaf(TreeNode node) {
+        return node == null || node.left == null && node.right == null;
     }
 }
