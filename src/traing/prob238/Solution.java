e1aac34f88fed90beeb1deee9752145ec3c92713
Ghost141 <kidd747@gmail.com>
2017-08-14 21:29:19 +0800

    #238 Accepted

---
+++ src/traing/prob238/Solution.java
@@ -0,0 +1,34 @@
+package traing.prob238;
+
+import traing.Utils;
+
+/**
+ * Product of Array Except Self
+ * Link: https://leetcode.com/problems/product-of-array-except-self/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 8/14/17
+ */
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        Utils.printai(s.productExceptSelf(new int[] {1, 0}));
+    }
+}
