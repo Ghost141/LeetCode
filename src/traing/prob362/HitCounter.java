baf86819509f8b1c962b2628178d6da8c00f0ff9
Ghost141 <kidd747@gmail.com>
2017-08-22 18:59:26 +0800

    #362 Accepted Update the solution by using buckets.

--- src/traing/prob362/HitCounter.java
+++ src/traing/prob362/HitCounter.java
@@ -1,24 +1,24 @@
 package traing.prob362;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Design Hit Counter
  * Link: https://leetcode.com/problems/design-hit-counter/description/
  *
  * @author zhaokai
  * @version 1.0
+ * @version 1.1 - Using buckets to record data.
  * @since 1.0 - 8/22/17
  */
 public class HitCounter {
-    private Map<Integer, Integer> map;
+    private int[] times;
+    private int[] hits;
 
     /**
      * Initialize your data structure here.
      */
     public HitCounter() {
-        this.map = new HashMap<>();
+        this.times = new int[300];
+        this.hits = new int[300];
     }
 
     /**
@@ -27,7 +27,11 @@ public class HitCounter {
      * @param timestamp - The current timestamp (in seconds granularity).
      */
     public void hit(int timestamp) {
-        map.put(timestamp, map.getOrDefault(timestamp, 0) + 1);
+        int ind = timestamp % 300;
+        if (this.times[ind] != timestamp) {
+            this.times[ind] = timestamp;
+            this.hits[ind] = 1;
+        } else this.hits[ind]++;
     }
 
     /**
@@ -38,7 +42,11 @@ public class HitCounter {
     public int getHits(int timestamp) {
         int res = 0;
         for (int i = 0; i < 300; i++)
-            if (map.containsKey(timestamp - i)) res += map.get(timestamp - i);
+        {
+            if (timestamp - this.times[i] < 300) {
+                res += this.hits[i];
+            }
+        }
         return res;
     }
 }
