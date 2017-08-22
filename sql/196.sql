4728f91a2d6c28d9737b8215101e7901a3099535
Ghost141 <kidd747@gmail.com>
2017-08-22 23:45:43 +0800

    #196 Accepted

---
+++ sql/196.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Person (
+  Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  Email VARCHAR(200)
+);
+
+DELETE p1 FROM Person p1
+INNER JOIN Person p2 ON p1.Email = p2.Email AND p1.Id > p2.Id
