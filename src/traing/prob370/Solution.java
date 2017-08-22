1fe3608fb815bcb4854a932f8b06ef0adbecad8f
Ghost141 <kidd747@gmail.com>
2017-08-22 16:04:05 +0800

    #370 Accepted

---
+++ src/traing/prob370/Solution.java
@@ -0,0 +1,35 @@
+package traing.prob370;
+
+import traing.Utils;
+import utils.DataGenerator;
+
+/**
+ * Range Addition
+ * Link: https://leetcode.com/problems/range-addition/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 8/22/17
+ */
+public class Solution {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        int[] res = new int[length];
+        for (int[] up : updates) {
+            res[up[0]] += up[2];
+            if (up[1] != length - 1)
+                res[up[1] + 1] += -up[2];
+        }
+        int sum = 0;
+        for (int i = 0; i < res.length; i++) {
+            sum += res[i];
+            res[i] = sum;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        Utils.printai(s.getModifiedArray(10, DataGenerator.createIntMatrix("2,4,6|5,6,8|1,9,-4")));
+    }
+}
