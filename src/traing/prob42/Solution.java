d3d6c6337f6f3544217fe33bf1f39a115f4821e9
Ghost141 <kidd747@gmail.com>
2017-08-23 16:59:26 +0800

    #42 Accepted

---
+++ src/traing/prob42/Solution.java
@@ -0,0 +1,38 @@
+package traing.prob42;
+
+/**
+ * Trapping Rain Water
+ * Link: https://leetcode.com/problems/trapping-rain-water/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 8/23/17
+ */
+public class Solution {
+    public int trap(int[] height) {
+        if (height.length < 3) return 0;
+        int area = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (leftMax < height[left]) {
+                    leftMax = height[left];
+                } else {
+                    area += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (rightMax < height[right]) {
+                    rightMax = height[right];
+                } else {
+                    area += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return area;
+    }
+}
