4c925bcc18a309c8bc2b119b70889fa9e7ff0021
Ghost141 <kidd747@gmail.com>
2017-08-24 17:33:13 +0800

    Add new method for ListNode data structure

--- src/utils/DataGenerator.java
+++ src/utils/DataGenerator.java
@@ -1,10 +1,13 @@
 package utils;
 
+import traing.ListNode;
+
 /**
  * Test data generator
  *
  * @author zhaokai
  * @version 1.0
+ * @version 1.1 - Add ListNode builder.
  * @since 1.0 - 8/17/17
  */
 public class DataGenerator {
@@ -40,5 +43,20 @@ public class DataGenerator {
             matrix[i] = intRow;
         }
         return matrix;
+    }
+
+    public static ListNode buildListNode(String s) {
+        String[] nodes = s.split("-");
+        if (nodes.length == 0) return null;
+        ListNode fakeHead = new ListNode(-1);
+        ListNode head = new ListNode(Integer.valueOf(nodes[0].trim()));
+        fakeHead.next = head;
+
+        for (int i = 1; i < nodes.length; i++) {
+            ListNode n = new ListNode(Integer.valueOf(nodes[i].trim()));
+            head.next = n;
+            head = n;
+        }
+        return fakeHead.next;
     }
 }
