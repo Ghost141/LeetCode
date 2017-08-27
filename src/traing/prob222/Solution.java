8ce967b377ecbc46220912291d5ad88e35829243
Ghost141 <kidd747@gmail.com>
2017-08-28 01:03:32 +0800

    #222 Accepted The calculation is finally correct

--- src/traing/prob222/Solution.java
+++ src/traing/prob222/Solution.java
@@ -13,12 +13,18 @@ import traing.TreeNode;
 public class Solution {
     public int countNodes(TreeNode root) {
         int h = getHeight(root);
-        return h == -1 ? 0 : (getHeight(root.right) < h ?
-                (1 << h) + countNodes(root.right)
-                : (1 << h - 1) + countNodes(root.left));
+        return h == -1 ? 0 : (getHeight(root.right) == h - 1 ?
+                ((1 << h) + countNodes(root.right))
+                : ((1 << h - 1) + countNodes(root.left)));
     }
 
     private int getHeight(TreeNode node) {
         return node == null ? -1 : 1 + getHeight(node.left);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.countNodes(new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3))));
     }
 }
