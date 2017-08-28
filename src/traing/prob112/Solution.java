3be489411e39069470ad5cf4fc70f69a7c5b8674
Ghost141 <kidd747@gmail.com>
2017-08-28 22:38:19 +0800

    #112 Accepted Redo the Path Sum problem

--- src/traing/prob112/Solution.java
+++ src/traing/prob112/Solution.java
@@ -2,27 +2,20 @@ package traing.prob112;
 
 import traing.TreeNode;
 
+/**
+ * Path Sum
+ * Link: https://leetcode.com/problems/path-sum/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 8/28/17
+ */
 public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
-        return root != null && (hasPathSumFrom(root, sum));
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return root.val == sum;
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 
-    private boolean hasPathSumFrom(TreeNode root, int sum) {
-        if (root == null) {
-            return sum == 0;
-        } else if (root.right != null || root.left != null) {
-            return hasPathSumFrom(root.left, sum - root.val) || hasPathSumFrom(root.right, sum - root.val);
-        } else {
-            return root.val == sum;
-        }
-    }
-
-    public static void main(String[] args) {
-        final Solution s = new Solution();
-        TreeNode n1 = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        n1.left = n2;
-
-        s.hasPathSum(n1, 1);
-    }
 }
