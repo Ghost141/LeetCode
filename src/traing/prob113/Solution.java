cd93e90ad4ed565966373d04fb3b7ff656b8a17b
Ghost141 <kidd747@gmail.com>
2017-08-29 00:50:52 +0800

    #113 Accepted Accepted but a little bit ugly.

--- src/traing/prob113/Solution.java
+++ src/traing/prob113/Solution.java
@@ -3,28 +3,50 @@ package traing.prob113;
 import traing.TreeNode;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
+/**
+ * Path Sum II
+ * Link: https://leetcode.com/problems/path-sum-ii/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 8/28/17
+ */
 public class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-
-        return null;
+        List<List<Integer>> list = new ArrayList<>();
+        sum(list, new ArrayList<>(), root, sum);
+        return list;
     }
 
-    private List<List<Integer>> pathSumFrom(TreeNode root, int sum) {
-        if (root == null) {
-
+    private void sum(List<List<Integer>> list, List<Integer> path, TreeNode root, int remain) {
+        if (root == null) return;
+        else if (remain == root.val && root.left == null && root.right == null) {
+            path.add(root.val);
+            list.add(new ArrayList<>(path));
+            path.remove(path.size() - 1);
+        } else {
+            path.add(root.val);
+            sum(list, path, root.left, remain - root.val);
+            sum(list, path, root.right, remain - root.val);
+            path.remove(path.size() - 1);
         }
-        return null;
     }
 
-    private List<List<Integer>> merge(List<List<Integer>> l1, List<List<Integer>> l2) {
-        List<List<Integer>> result = new ArrayList<>();
-        Collections.addAll(l1);
-        Collections.addAll(l2);
-        result.addAll(l1);
-        result.addAll(l2);
-        return result;
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode tree = new TreeNode(5,
+                new TreeNode(4,
+                        new TreeNode(11,
+                                new TreeNode(7), new TreeNode(2)),
+                        null),
+                new TreeNode(8,
+                        new TreeNode(13),
+                        new TreeNode(4,
+                                new TreeNode(5), new TreeNode(1))));
+
+//        System.out.println(s.pathSum(tree, 22));
+        System.out.println(s.pathSum(new TreeNode(-2, null, new TreeNode(-3)), -5));
     }
 }
