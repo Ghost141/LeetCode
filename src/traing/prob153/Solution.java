37bea687604cbf63beaa3002b55327012bdb360b
Ghost141 <kidd747@gmail.com>
2017-08-29 18:39:08 +0800

    Update problem 153 solution file header

--- src/traing/prob153/Solution.java
+++ src/traing/prob153/Solution.java
@@ -1,5 +1,13 @@
 package traing.prob153;
 
+/**
+ * Find Minimum in Rotated Sorted Array
+ * Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 6/27/17
+ */
 public class Solution {
     public int findMin(int[] nums) {
         int start = 0, end = nums.length - 1, middle = (start + end) / 2;
