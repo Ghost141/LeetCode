1e51b23b5393d131e439803f2ab70ea5d474b819
Ghost141 <kidd747@gmail.com>
2017-08-29 15:13:07 +0800

    Add Interval class

---
+++ src/traing/Interval.java
@@ -0,0 +1,22 @@
+package traing;
+
+/**
+ * The common interval class.
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 8/29/17
+ */
+public class Interval {
+    public int start;
+    public int end;
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
