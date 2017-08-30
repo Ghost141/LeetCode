91e2508b07231baa229bbc73c5d8cdb02455f19c
Ghost141 <kidd747@gmail.com>
2017-08-30 17:35:10 +0800

    #645 Accepted

---
+++ src/traing/prob645/Solution.java
@@ -0,0 +1,40 @@
+package traing.prob645;
+
+import traing.Utils;
+import utils.DataGenerator;
+
+/**
+ * Set Mismatch
+ * Link: https://leetcode.com/problems/set-mismatch/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 8/30/17
+ */
+public class Solution {
+
+    public int[] findErrorNums(int[] nums) {
+        int dup = 0, mis = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int ind = Math.abs(nums[i]) - 1;
+            if (nums[ind] < 0) {
+                dup = ind + 1;
+                continue;
+            }
+            nums[ind] = -nums[ind];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                mis = i + 1;
+            }
+        }
+        return new int[] {dup, mis};
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        Utils.printai(s.findErrorNums(DataGenerator.createIntArray("1,2,2,4")));
+        Utils.printai(s.findErrorNums(DataGenerator.createIntArray("2,3,2")));
+    }
+}
