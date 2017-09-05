9cd85d8bf2c07190bfb9d3dedfccce596ad947fd
Ghost141 <kidd747@gmail.com>
2017-09-05 21:18:52 +0800

    Update solution 389 file header

--- src/traing/prob389/Solution.java
+++ src/traing/prob389/Solution.java
@@ -2,9 +2,13 @@ package traing.prob389;
 
 /**
  * Find the Difference
+ * Link: https://leetcode.com/problems/find-the-difference/description/
  *
  * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 7/20/17
  */
+
 public class Solution {
     public char findTheDifference(String s, String t) {
         char c = 0;
