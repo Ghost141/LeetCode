18a46cb1a3708c242bf3be3445abb111b7e01796
Ghost141 <kidd747@gmail.com>
2017-09-05 14:26:27 +0800

    Problem 437 Path Sum III prefix sum version solution

--- src/traing/prob437/Solution.java
+++ src/traing/prob437/Solution.java
@@ -2,24 +2,45 @@ package traing.prob437;
 
 import traing.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Path Sum III
  * Link: https://leetcode.com/problems/path-sum-iii/description/
  *
  * @author zhaokai
- * @version 1.1
+ * @version 1.1 - Prefix sum version.
  * @since 1.0 - 8/29/17
  */
 public class Solution {
-    public int pathSum(TreeNode root, int sum) {
-        if (root == null) return 0;
+//    public int pathSum(TreeNode root, int sum) {
+//        if (root == null) return 0;
+//
+//        return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+//    }
+//    private int pathSumFrom(TreeNode root, int sum) {
+//        if (root == null) return 0;
+//        return (root.val == sum ? 1 : 0) + pathSumFrom(root.left, sum - root.val)
+//                + pathSumFrom(root.right, sum - root.val);
+//    }
 
-        return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    public int pathSum(TreeNode root, int sum) {
+        Map<Integer, Integer> prefixSumMap = new HashMap<>();
+        prefixSumMap.put(0, 1);
+
+        return helper(root, 0, prefixSumMap, sum);
     }
-    private int pathSumFrom(TreeNode root, int sum) {
-        if (root == null) return 0;
-        return (root.val == sum ? 1 : 0) + pathSumFrom(root.left, sum - root.val)
-                + pathSumFrom(root.right, sum - root.val);
+
+    private int helper(TreeNode node, int currentSum, Map<Integer, Integer> prefixSumMap, int target) {
+        if (node == null) return 0;
+        currentSum += node.val;
+        int res = prefixSumMap.getOrDefault(currentSum - target, 0);
+        prefixSumMap.put(currentSum, prefixSumMap.getOrDefault(currentSum, 0) + 1);
+
+        res += helper(node.left, currentSum, prefixSumMap, target) + helper(node.right, currentSum, prefixSumMap, target);
+        prefixSumMap.put(currentSum, prefixSumMap.get(currentSum) - 1);
+        return res;
     }
 
     public static void main(String[] args) {
