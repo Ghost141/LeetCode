6ba3ff0e9c61b6dc7a68ce8c5501f08e253615c6
Ghost141 <kidd747@gmail.com>
2017-09-08 15:59:03 +0800

    #277 Accepted

---
+++ src/traing/prob277/Solution.java
@@ -0,0 +1,35 @@
+package traing.prob277;
+
+import traing.Relation;
+import utils.DataGenerator;
+
+/**
+ * Find the Celebrity
+ * Link: https://leetcode.com/problems/find-the-celebrity/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 9/8/17
+ */
+public class Solution extends Relation {
+
+    public Solution(boolean[][] matrix) {
+        super(matrix);
+    }
+
+    public int findCelebrity(int n) {
+        int can = 0;
+        for (int i = 0; i < n; i++)
+            if (knows(can, i)) can = i;
+        for (int i = 0; i < n; i++)
+            if (i != can && (!knows(i, can) || knows(can, i))) return -1;
+        return can;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution(DataGenerator.createBooleanMatrix("t,t|f,t"));
+
+
+        System.out.println(s.findCelebrity(2));
+    }
+}
