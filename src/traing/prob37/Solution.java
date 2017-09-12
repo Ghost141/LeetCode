f936a61add9dcd08aa391f791621a68fb2a42c83
Ghost141 <kidd747@gmail.com>
2017-09-12 18:23:30 +0800

    #37 Accepted

---
+++ src/traing/prob37/Solution.java
@@ -0,0 +1,47 @@
+package traing.prob37;
+
+/**
+ * Sudoku Solver
+ * Link: https://leetcode.com/problems/sudoku-solver/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 9/12/17
+ */
+public class Solution {
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0) return;
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (solve(board)) {
+                                return true;
+                            } else board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] != '.' && board[i][col] == c) return false;
+            if (board[row][i] != '.' && board[row][i] == c) return false;
+            int box = 3 * (row / 3) + i / 3;
+            int box2 = 3 * (col / 3) + i % 3;
+            if (board[box][box2] != '.' &&
+                    board[box][box2] == c) return false;
+        }
+        return true;
+    }
+}
