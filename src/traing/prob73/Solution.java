36ef1e724c19833cb46c549998375fde5696c8b5
Ghost141 <kidd747@gmail.com>
2017-09-14 18:09:56 +0800

    #73 Accepted

---
+++ src/traing/prob73/Solution.java
@@ -0,0 +1,34 @@
+package traing.prob73;
+
+/**
+ * Set Matrix Zeroes
+ * Link: https://leetcode.com/problems/set-matrix-zeroes/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 9/14/17
+ */
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstRow = false, firstCol = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == 0 && matrix[0][j] == 0) firstRow = true;
+                if (j == 0 && matrix[i][0] == 0) firstCol = true;
+                if (matrix[i][j] == 0) matrix[i][0] = matrix[0][j] = 0;
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+        if (firstRow) {
+            for (int i = 0; i < matrix[0].length; i++) matrix[0][i] = 0;
+        }
+        if (firstCol) {
+            for (int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+        }
+    }
+}
