14f64201c9bb6da33e30f0f76d4bdd236c23c719
Ghost141 <kidd747@gmail.com>
2017-09-25 23:52:52 +0800

    A much more fast and concise version for problem 299.

--- src/traing/prob299/Solution.java
+++ src/traing/prob299/Solution.java
@@ -1,34 +1,29 @@
 package traing.prob299;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Bulls and Cows
  * Link: https://leetcode.com/problems/bulls-and-cows/description/
  *
  * @author zhaokai
  * @version 1.0
+ * @version 1.1 - One pass and no map involved solution.
  * @since 1.0 - 9/25/17
  */
 public class Solution {
     public String getHint(String secret, String guess) {
-        int A = 0;
-        int B = 0;
-        Map<Character, Integer> secretMap = new HashMap<>();
-        Map<Character, Integer> guessMap = new HashMap<>();
+        int bulls = 0;
+        int cows = 0;
+        int[] nums = new int[10];
         for (int i = 0; i < secret.length(); i++) {
             char s = secret.charAt(i);
             char g = guess.charAt(i);
-            if (s == g) A++;
+            if (s == g) bulls++;
             else {
-                secretMap.put(s, secretMap.getOrDefault(s, 0) + 1);
-                guessMap.put(g, guessMap.getOrDefault(g, 0) + 1);
+                if (nums[s]++ < 0) cows++;
+                if (nums[g]-- > 0) cows++;
             }
         }
-        for (Map.Entry<Character, Integer> entry : guessMap.entrySet())
-            if (secretMap.containsKey(entry.getKey())) B += Math.min(secretMap.get(entry.getKey()), entry.getValue());
-        return A + "A" + B + "B";
+        return bulls + "A" + cows + "B";
     }
 
     public static void main(String[] args) {
