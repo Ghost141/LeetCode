3bbe5e527f8985035eff4443e459849083b2d85e
Ghost141 <kidd747@gmail.com>
2017-10-10 20:48:39 +0800

    #395 Accepted

---
+++ src/traing/prob395/Solution.java
@@ -0,0 +1,41 @@
+package traing.prob395;
+
+/**
+ * Link:
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 10/10/17
+ */
+public class Solution {
+    public int longestSubstring(String s, int k) {
+        char[] str = s.toCharArray();
+        return helper(str, 0, s.length(), k);
+    }
+
+    private int helper(char[] str, int start, int end, int k) {
+        if (end - start < k) return 0;//substring length shorter than k.
+        int[] count = new int[26];
+        for (int i = start; i < end; i++) {
+            count[str[i] - 'a']++;
+        }
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (count[c - 'a'] < k && count[c - 'a'] != 0) { //count[i] = 0 => c does not exist in the string, skip it.
+                for (int j = start; j < end; j++) {
+                    if (str[j] == c) {
+                        int left = helper(str, start, j, k);
+                        int right = helper(str, j + 1, end, k);
+                        return Math.max(left, right);
+                    }
+                }
+            }
+        }
+        return end - start;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.longestSubstring("aaabb", 3));
+    }
+}
