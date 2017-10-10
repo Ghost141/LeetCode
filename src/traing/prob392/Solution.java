592a73914f2879408cf75ceaf8adaa44bc6accb4
Ghost141 <kidd747@gmail.com>
2017-10-10 15:47:15 +0800

    #392 Accepted A binary search version

--- src/traing/prob392/Solution.java
+++ src/traing/prob392/Solution.java
@@ -1,11 +1,15 @@
 package traing.prob392;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Is Subsequence
  * Link: https://leetcode.com/problems/is-subsequence/description/
  *
  * @author zhaokai
- * @version 1.0
+ * @version 1.1 - Binary Search version.
  * @since 1.0 - 10/10/17
  */
 public class Solution {
@@ -17,6 +21,30 @@ public class Solution {
             indS++;
         }
         return true;
+    }
+
+    private boolean binarySearch(String s, String t) {
+        List<Integer>[] idx = new List[26];
+        for (int i = 0; i < t.length(); i++) {
+            int ind = ind(t.charAt(i));
+            if (idx[ind] == null) idx[ind] = new ArrayList<>();
+            idx[ind].add(i);
+        }
+
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int ind = ind(s.charAt(i));
+            if (idx[ind] == null) return false;
+            int j = Collections.binarySearch(idx[ind], prev);
+            if (j < 0) j = -(j + 1);
+            if (j == idx[ind].size()) return false;
+            prev = idx[ind].get(j) + 1;
+        }
+        return true;
+    }
+
+    private int ind(char c) {
+        return c - 'a';
     }
 
     public static void main(String[] args) {
