a802e6b1b4b9691be95bb9088c8ebaf7c1410a5b
Ghost141 <kidd747@gmail.com>
2017-10-11 22:07:01 +0800

    Simplify the linear scan solution

--- src/traing/prob162/Solution.java
+++ src/traing/prob162/Solution.java
@@ -7,23 +7,18 @@ import utils.DataGenerator;
  * Link: https://leetcode.com/problems/find-peak-element/description/
  *
  * @author zhaokai
- * @version 1.0
+ * @version 1.0 - Linear Scan solution
  * @since 1.0 - 9/19/17
  */
 public class Solution {
     public int findPeakElement(int[] nums) {
-        if (nums.length == 0) return -1;
-        else if (nums.length == 1) return 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                if (nums[i] > nums[i + 1]) return i;
-            } else if (i == nums.length - 1) {
-                if (nums[i] > nums[i - 1]) return i;
-            } else {
-                if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
-            }
-        }
-        return -1;
+        return linearScan(nums);
+    }
+
+    private int linearScan(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++)
+            if (nums[i] > nums[i + 1]) return i;
+        return nums.length - 1;
     }
 
     public static void main(String[] args) {
