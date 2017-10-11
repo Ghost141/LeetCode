d744067089bccb631c45e44965600b4a381b0da2
Ghost141 <kidd747@gmail.com>
2017-10-11 15:24:59 +0800

    #417 Accepted Brilliant idea about flip the flow direction.

--- src/traing/prob417/Solution.java
+++ src/traing/prob417/Solution.java
@@ -4,9 +4,7 @@ import utils.DataGenerator;
 import utils.PrintUtils;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Pacific Atlantic Water Flow
@@ -17,8 +15,6 @@ import java.util.Set;
  * @since 1.0 - 10/11/17
  */
 public class Solution {
-    private static final int PACIFIC = 0;
-    private static final int ATLANTIC = 1;
     private static final int[][] DIRECTIONS = {
             {0, 1}, {0, -1}, {1, 0}, {-1, 0}
     };
@@ -27,39 +23,33 @@ public class Solution {
         if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0)
             return new ArrayList<>();
         List<int[]> res = new ArrayList<>();
-        boolean[] reached = new boolean[2];
-        Set<Integer> cache = new HashSet<>();
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                int[] point = {i, j};
-                if (dfs(point, matrix, new boolean[matrix.length][matrix[i].length], reached, cache)) {
-                    res.add(point);
-                    cache.add(i * matrix[i].length + j);
-                }
-                reached[PACIFIC] = reached[ATLANTIC] = false;
-            }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] altantic = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dfs(matrix, pacific, i, 0, Integer.MIN_VALUE);
+            dfs(matrix, altantic, i, n - 1, Integer.MIN_VALUE);
         }
+
+        for (int i = 0; i < n; i++) {
+            dfs(matrix, pacific, 0, i, Integer.MIN_VALUE);
+            dfs(matrix, altantic, m - 1, i, Integer.MIN_VALUE);
+        }
+
+
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[i].length; j++)
+                if (pacific[i][j] && altantic[i][j]) res.add(new int[]{i, j});
         return res;
     }
 
-    private boolean dfs(int[] point, int[][] matrix, boolean[][] visited, boolean[] reached, Set<Integer> cache) {
-        int p0 = point[0];
-        int p1 = point[1];
-        if (cache.contains(p0 * matrix[0].length + p1)) return true;
-        if (p0 == 0 || p1 == 0) reached[PACIFIC] = true;
-        if (p0 == matrix.length - 1 || p1 == matrix[0].length - 1) reached[ATLANTIC] = true;
-        if (reached[PACIFIC] && reached[ATLANTIC]) return true;
-        for (int[] dir : DIRECTIONS) {
-            int x = dir[0] + p0;
-            int y = dir[1] + p1;
-            if (x >= 0 && y >= 0 && x < matrix.length && y < matrix[0].length && !visited[x][y]
-                    && matrix[x][y] <= matrix[p0][p1]) {
-                visited[x][y] = true;
-                if (dfs(new int[]{x, y}, matrix, visited, reached, cache)) return true;
-                visited[x][y] = false;
-            }
+    private void dfs(int[][] matrix, boolean[][] visited, int row, int col, int height) {
+        if (row >= 0 && col >= 0 && row < matrix.length && col < matrix[0].length && matrix[row][col] >= height && !visited[row][col]) {
+            visited[row][col] = true;
+            for (int[] dir : DIRECTIONS) dfs(matrix, visited, row + dir[0], col + dir[1], matrix[row][col]);
         }
-        return false;
     }
 
     public static void main(String[] args) {
