e3ab7845c3f606bda7a5b96d685d625c523c43b8
Ghost141 <kidd747@gmail.com>
2017-10-12 16:50:22 +0800

    #496 Accepted Stack solution.

--- src/traing/prob496/Solution.java
+++ src/traing/prob496/Solution.java
@@ -1,5 +1,7 @@
 package traing.prob496;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,10 +12,15 @@ import java.util.Map;
  * @author zhaokai
  * @version 1.0 - Failed to implement.
  * @version 1.1 - Better brute force solution by using Hash Table.
+ * @version 1.2 - Using Stack.
  * @since 1.0 - 6/20/17
  */
 public class Solution {
     public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        return bruteForce(findNums, nums);
+    }
+
+    private int[] bruteForce(int[] findNums, int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         int[] res = new int[findNums.length];
         for (int i = 0; i < nums.length; i++) map.put(nums[i], i);
@@ -27,6 +34,20 @@ public class Solution {
             }
             res[i] = ind == -1 ? -1 : nums[ind];
         }
+
+        return res;
+    }
+
+    private int[] stack(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[findNums.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num) map.put(stack.pop(), num);
+            stack.push(num);
+        }
+        for (int i = 0; i < findNums.length; i++) res[i] = map.getOrDefault(findNums[i], -1);
 
         return res;
     }
