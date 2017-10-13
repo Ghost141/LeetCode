f72d37e1a5dfa189922bdb44177296d7304a408d
Ghost141 <kidd747@gmail.com>
2017-10-14 01:27:04 +0800

    Update problem 237 submission

--- src/traing/prob237/Solution.java
+++ src/traing/prob237/Solution.java
@@ -2,11 +2,17 @@ package traing.prob237;
 
 import traing.common.ListNode;
 
+/**
+ * Delete Node in a Linked List
+ * Link: https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 7/15/17
+ */
 public class Solution {
     public void deleteNode(ListNode node) {
-        if (node.next != null) {
-            node.val = node.next.val;
-            node.next = node.next.next;
-        }
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
