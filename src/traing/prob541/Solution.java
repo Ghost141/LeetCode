eeb7e9dc1f3b54f53149fc5395e5839189864743
Ghost141 <kidd747@gmail.com>
2017-10-16 19:04:22 +0800

    #541 Accepted

--- src/traing/prob541/Solution.java
+++ src/traing/prob541/Solution.java
@@ -5,10 +5,22 @@ package traing.prob541;
  * Link: https://leetcode.com/problems/reverse-string-ii/tabs/description
  *
  * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 10/16/17
  */
 public class Solution {
     public String reverseStr(String s, int k) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < s.length(); i += 2 * k) reverse(chars, i, i + k);
+        return new String(chars);
+    }
 
-        return null;
+    private void reverse(char[] chars, int i, int j) {
+        j = Math.min(chars.length, j) - 1;
+        while (i < j) {
+            char temp = chars[i];
+            chars[i++] = chars[j];
+            chars[j--] = temp;
+        }
     }
 }
