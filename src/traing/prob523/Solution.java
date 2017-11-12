39b2e56fe11d56e57af4c70af7bf355788bd99e4
Ghost141 <kidd747@gmail.com>
2017-11-12 19:35:17 +0800

    #523 Accepted A better solution based on hash map. Actually this solution shows a better understanding of mod operation.

--- src/traing/prob523/Solution.java
+++ src/traing/prob523/Solution.java
@@ -1,5 +1,8 @@
 package traing.prob523;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Continuous Subarray Sum
  * Link: https://leetcode.com/problems/continuous-subarray-sum/description/
@@ -10,6 +13,24 @@ package traing.prob523;
  */
 public class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
+        return betterBruteForce(nums, k);
+    }
+
+    private boolean hashMapSolution(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (k != 0) sum %= k;
+            if (map.containsKey(sum)) {
+                if (i - map.get(sum) > 1) return true;
+            } else map.put(sum, i);
+        }
+        return false;
+    }
+
+    private boolean betterBruteForce(int[] nums, int k) {
         int[] sum = new int[nums.length + 1];
         for (int i = 0; i < nums.length; i++) sum[i + 1] += sum[i] + nums[i];
         for (int i = 0; i < nums.length - 1; i++) {
