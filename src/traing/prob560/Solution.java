d2813de0070734f22676596c7011c5734714be85
Ghost141 <kidd747@gmail.com>
2017-11-12 19:54:53 +0800

    #560 Accepted

---
+++ src/traing/prob560/Solution.java
@@ -0,0 +1,26 @@
+package traing.prob560;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Subarray Sum Equals K
+ * Link: https://leetcode.com/problems/subarray-sum-equals-k/description/
+ *
+ * @author zhaokai
+ * @version 1.0
+ * @since 1.0 - 11/12/17
+ */
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            count += map.getOrDefault(sum - k, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
