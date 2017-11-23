280b021ad69ee217f784f49ea3c0056f19044535
Ghost141 <kidd747@gmail.com>
2017-11-23 20:44:25 +0800

    #435 Accepted

--- src/traing/prob435/Solution.java
+++ src/traing/prob435/Solution.java
@@ -20,6 +20,26 @@ public class Solution {
         return bruteForce(intervals);
     }
 
+    private int dpBySortingEnd(Interval[] intervals) {
+        if (intervals.length <= 1) return intervals.length;
+        Arrays.sort(intervals, END_COMPARATOR);
+        int[] dp = new int[intervals.length];
+        dp[0] = 1;
+        int ans = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            int max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (!(intervals[j].end > intervals[i].start)) {
+                    max = Math.max(max, dp[j]);
+                    break;
+                }
+            }
+            dp[i] = Math.max(max + 1, dp[i - 1]);
+            ans = Math.max(ans, dp[i]);
+        }
+        return intervals.length - ans;
+    }
+
     private int bruteForce(Interval[] intervals) {
         Arrays.sort(intervals, START_COMPARATOR);
         return remove(intervals, -1, 0);
